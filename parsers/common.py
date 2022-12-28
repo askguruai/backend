a0ba9b22d899918b40203fccd1b9980b64fa2240
Alexander Smirnov <furiousteabag@gmail.com>
2022-12-28 05:09:10 -0500

    fix typo in var name

--- parsers/common.py
+++ parsers/common.py
@@ -1,3 +1,4 @@
+import logging
 from nltk import tokenize
 from typing import List
 
@@ -6,14 +7,14 @@ def text_to_sentences(text: str) -> List[str]:
     return tokenize.sent_tokenize(text)
 
 
-def chunkise_sentences(sentences: List[str], chunk_size: int = 1024) -> List[str]:
+def chunkise_sentences(sentences: List[str], chunk_size: int) -> List[str]:
     chunks = []
-    current_cunk = ""
+    current_chunk = ""
     for sent in sentences:
-        if len(current_cunk) + len(sent) <= chunk_size:
-            current_cunk += sent
+        if len(current_chunk) + len(sent) <= chunk_size:
+            current_chunk += sent
         else:
-            chunks.append(current_cunk)
-            current_cunk = sent
-    chunks.append(current_cunk)
+            chunks.append(current_chunk)
+            current_chunk = sent
+    chunks.append(current_chunk)
     return chunks
