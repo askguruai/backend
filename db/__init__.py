f87ff7f222948af5d5f5750452b8d1adcb11fb08
Alexander Smirnov <furiousteabag@gmail.com>
2023-01-03 12:23:46 -0500

    add mongodb connection

---
+++ db/__init__.py
@@ -0,0 +1,21 @@
+import os
+import urllib
+
+import pymongo
+
+from utils import CONFIG
+
+HOST = CONFIG["mongo"]["host"]
+PORT = CONFIG["mongo"]["port"]
+DATABASE = CONFIG["mongo"]["db"]
+USER = os.environ["MONGO_INITDB_ROOT_USERNAME"]
+PASSWORD = urllib.parse.quote_plus(os.environ["MONGO_INITDB_ROOT_PASSWORD"])
+
+mongo_client = pymongo.MongoClient(
+    host=str(HOST) + ":" + str(PORT),
+    serverSelectionTimeoutMS=3000,
+    username=USER,
+    password=PASSWORD,
+)
+
+DB = mongo_client[DATABASE]
