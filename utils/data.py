2285d0096f1d08df5ca9f9aa6b54086d11f7ff19
Alexander Smirnov <furiousteabag@gmail.com>
2023-01-03 12:24:09 -0500

    add SetReactionRequest dataclass

--- utils/data.py
+++ utils/data.py
@@ -1,6 +1,14 @@
+from typing import Optional
+
 from pydantic import BaseModel
 
 
 class TextRequest(BaseModel):
     text_input: str
     query: str
+
+
+class SetReactionRequest(BaseModel):
+    request_id: str
+    like: bool
+    comment: Optional[str] = None
