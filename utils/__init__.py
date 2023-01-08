b30c0278bc1346b3d55eae25228d419197bc69b0
Sergei Gorbatiuk <spgorbatiuk@gmail.com>
2023-01-08 16:55:51 +0000

    fixing import

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,5 @@
-import json
 from configparser import ConfigParser
-from storage_dispatcher import StorageDispatcher
+from utils.storage_dispatcher import StorageDispatcher
 
 CONFIG = ConfigParser()
 CONFIG.read("./config.ini")
