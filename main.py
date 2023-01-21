07c6abadace4e160d66159717d1a121b61c6371a
Alexander Smirnov <furiousteabag@gmail.com>
2023-01-21 20:51:24 +0700

    change error handling

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from typing import List, Union
 import bson
 import uvicorn
 from bson.objectid import ObjectId
-from fastapi import FastAPI, File, Request, UploadFile, status
+from fastapi import FastAPI, File, HTTPException, Request, Response, UploadFile, status
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse, RedirectResponse
 from pymongo.collection import ReturnDocument
@@ -14,8 +14,16 @@ from pymongo.collection import ReturnDocument
 from handlers import DocumentHandler, LinkHandler, PDFUploadHandler, TextHandler
 from parsers import DocumentParser, LinkParser, TextParser
 from utils import CONFIG, DB
-from utils.api import DocumentRequest, LinkRequest, SetReactionRequest, TextRequest
-from utils.errors import InvalidDocumentIdError, RequestDataModelMismatchError, CoreMLError
+from utils.api import (
+    DocumentRequest,
+    GetAnswerResponse,
+    HTTPExceptionResponse,
+    LinkRequest,
+    SetReactionRequest,
+    TextRequest,
+    UploadDocumentResponse,
+)
+from utils.errors import CoreMLError, InvalidDocumentIdError, RequestDataModelMismatchError
 from utils.logging import run_uvicorn_loguru
 
 app = FastAPI()
@@ -71,50 +79,84 @@ def log_get_answer(
     return str(request_id)
 
 
-@app.post("/get_answer/text")
+@app.post(
+    "/get_answer/text",
+    response_model=GetAnswerResponse,
+    responses={status.HTTP_500_INTERNAL_SERVER_ERROR: {"model": HTTPExceptionResponse}},
+)
 async def get_answer_text(text_request: TextRequest, request: Request):
     try:
         answer, context, document_id = TEXT_HANDLER.get_answer(text_request)
     except CoreMLError as e:
-        logging.error(f"Error: {e.__class__.__name__}")
-        return e.response()
+        logging.error(e)
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=str(e),
+        )
     request_id = log_get_answer(answer, context, document_id, text_request.query, request)
-    return {"data": answer, "request_id": request_id}
+    return GetAnswerResponse(answer=answer, request_id=request_id)
 
 
-@app.post("/get_answer/link")
+@app.post(
+    "/get_answer/link",
+    response_model=GetAnswerResponse,
+    responses={status.HTTP_500_INTERNAL_SERVER_ERROR: {"model": HTTPExceptionResponse}},
+)
 async def get_answer_link(link_request: LinkRequest, request: Request):
     try:
         answer, context, document_id = LINK_HANDLER.get_answer(link_request)
     except CoreMLError as e:
-        logging.error(f"Error: {e.__class__.__name__}")
-        return e.response()
+        logging.error(e)
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=str(e),
+        )
     request_id = log_get_answer(answer, context, document_id, link_request.query, request)
-    return {"data": answer, "request_id": request_id}
+    return GetAnswerResponse(answer=answer, request_id=request_id)
 
 
-@app.post("/get_answer/document")
+@app.post(
+    "/get_answer/document",
+    response_model=GetAnswerResponse,
+    responses={status.HTTP_500_INTERNAL_SERVER_ERROR: {"model": HTTPExceptionResponse}},
+)
 async def get_answer_document(document_request: DocumentRequest, request: Request):
     try:
         answer, context, info_source, document_ids = DOCUMENT_HANDLER.get_answer(document_request)
     except (InvalidDocumentIdError, RequestDataModelMismatchError, CoreMLError) as e:
-        logging.error(f"Error: {e.__class__.__name__}")
-        return e.response()
+        logging.error(e)
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=str(e),
+        )
     request_id = log_get_answer(answer, context, document_ids, document_request.query, request)
-    return {"data": answer, "request_id": request_id, "info_source": info_source}
+    return GetAnswerResponse(answer=answer, request_id=request_id, info_source=info_source)
 
 
-@app.post("/upload/pdf")
+@app.post(
+    "/upload/pdf",
+    response_model=UploadDocumentResponse,
+    responses={status.HTTP_500_INTERNAL_SERVER_ERROR: {"model": HTTPExceptionResponse}},
+)
 async def upload_pdf(file: UploadFile = File(...)):
     try:
         document_id = PDF_UPLOAD_HANDLER.process_file(file)
     except CoreMLError as e:
-        logging.error(f"Error: {e.__class__.__name__}")
-        return e.response()
-    return {"status": "success", "document_id": document_id}
+        logging.error(e)
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=str(e),
+        )
+    return UploadDocumentResponse(document_id=document_id)
 
 
-@app.post("/set_reaction")
+@app.post(
+    "/set_reaction",
+    responses={
+        status.HTTP_400_BAD_REQUEST: {"model": HTTPExceptionResponse},
+        status.HTTP_404_NOT_FOUND: {"model": HTTPExceptionResponse},
+    },
+)
 async def set_reaction(set_reaction_request: SetReactionRequest):
     row_update = {
         "like_status": set_reaction_request.like_status,
@@ -122,21 +164,24 @@ async def set_reaction(set_reaction_request: SetReactionRequest):
     }
 
     try:
-        status = DB[CONFIG["mongo"]["requests_collection"]].find_one_and_update(
+        db_status = DB[CONFIG["mongo"]["requests_collection"]].find_one_and_update(
             {"_id": ObjectId(set_reaction_request.request_id)},
             {"$set": row_update},
             return_document=ReturnDocument.AFTER,
         )
-        result = (
-            f"Row {set_reaction_request.request_id} was successfully updated"
-            if status
-            else f"Can't find row with id {set_reaction_request.request_id}"
-        )
+        if not db_status:
+            logging.error(f"Can't find row with id {set_reaction_request.request_id}")
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"Can't find row with id {set_reaction_request.request_id}",
+            )
     except bson.errors.InvalidId as e:
-        result = f"Provided id {set_reaction_request.request_id} has wrong format"
-
-    logging.info(result)
-    return {"result": result}
+        logging.error(e)
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=str(e),
+        )
+    return Response(status_code=status.HTTP_200_OK)
 
 
 if __name__ == "__main__":
