829c8162d6eea864ce4d0d90f818afcb83b07972
Alexander Smirnov <furiousteabag@gmail.com>
2023-01-21 20:49:52 +0700

    change ml requests error handling

--- utils/ml_requests.py
+++ utils/ml_requests.py
@@ -2,30 +2,28 @@ from typing import List, Union
 
 import numpy as np
 import requests
+from fastapi import status
 
 from utils import CONFIG
 from utils.errors import CoreMLError
 
 
 def get_embeddings(chunks: Union[str, List[str]]) -> List[np.ndarray]:
-    embeddings = []
-    req_data = {"input": chunks}
     response = requests.post(
         f"http://{CONFIG['coreml']['host']}:{CONFIG['coreml']['port']}/embeddings",
-        json=req_data,
-    ).json()
-    if response["status"] == "error":
-        raise CoreMLError(f"CoreML error: {response['message']}")
-    for emb in response["data"]:
-        embeddings.append(np.array(emb["embedding"]))
+        json={"input": chunks},
+    )
+    if response.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR:
+        raise CoreMLError(response.json()["detail"])
+    embeddings = [np.array(emb["embedding"]) for emb in response.json()["data"]]
     return embeddings
 
 
 def get_answer(context: str, query: str) -> str:
-    req_data = {"info": context, "query": query}
     response = requests.post(
-        f"http://{CONFIG['coreml']['host']}:{CONFIG['coreml']['port']}/completions", json=req_data
-    ).json()
-    if response["status"] == "error":
-        raise CoreMLError(f"CoreML error: {response['message']}")
-    return response["data"]
+        f"http://{CONFIG['coreml']['host']}:{CONFIG['coreml']['port']}/completions",
+        json={"info": context, "query": query},
+    )
+    if response.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR:
+        raise CoreMLError(response.json()["detail"])
+    return response.json()["data"]
