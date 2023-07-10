355382d559e72fd150d76e0c6af4ff3b9f48211f
Sergei Gorbatiuk <spgorbatiuk@gmail.com>
2023-07-10 11:04:36 -0400

    pdf parser

--- parsers/__init__.py
+++ parsers/__init__.py
@@ -3,3 +3,4 @@ from parsers.documents_parser import DocumentsParser
 from parsers.docx_parser_ import DocxParser
 from parsers.link_parser import LinkParser
 from parsers.text_parser import TextParser
+from parsers.pdf_parser import PdfParser
