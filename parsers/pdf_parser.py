59f835cfad97ac566d51be6055d6108f133e74f1
Sergei Gorbatiuk <spgorbatiuk@gmail.com>
2023-07-11 17:24:17 -0400

    missing file

---
+++ parsers/pdf_parser.py
@@ -0,0 +1,27 @@
+import os.path as osp
+import re
+from typing import List, Tuple
+
+import fitz
+
+from parsers.general_parser import GeneralParser
+from utils.tokenize_ import doc_to_chunks, doc_to_chunks2
+
+
+class PdfParser(GeneralParser):
+    def __init__(self, chunk_size: int):
+        super().__init__(chunk_size)
+        self.ld = None
+
+    def process_file(self, path) -> Tuple[List[str], str, dict]:
+        file_name = osp.splitext(osp.split(path)[1])[0]
+        meta = {"doc_title": file_name}
+        with fitz.open(path) as doc:
+            content = ""
+            for page in doc:
+                content += page.get_text()
+        content = re.sub(r"\.\.\.\.+", "...", content)
+        meta["security_groups"] = 2**63 - 1
+        chunks = doc_to_chunks2(content=content, title=file_name)
+        chunks = doc_to_chunks(content=content, title=file_name)
+        return chunks, content, meta
