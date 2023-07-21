58c6724abe11d504742decb5d59bb07a54402220
Alexander Smirnov <furiousteabag@gmail.com>
2023-07-21 15:49:39 +0300

    prepare for on prem deployment

--- utils/auth.py
+++ utils/auth.py
@@ -34,29 +34,8 @@ async def get_organization_token(
         None, description="Security groups associated with token. Leave blank for full access"
     ),
 ):
-    if password == os.environ["AUTH_COLLECTION_PASSWORD_ASKGURUPUBLIC"] and vendor != "askgurupublic":
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="You are only allowed to access organizations from askgurupublic vendor",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    if password == os.environ["AUTH_COLLECTION_PASSWORD_TADA"] and vendor != "tada":
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="You are only allowed to access organizations from tada vendor",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    if password == os.environ["AUTH_COLLECTION_PASSWORD_ONECLICKCX"] and vendor != "oneclickcx":
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="You are only allowed to access organizations from oneclickcx vendor",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
     if password not in [
         os.environ["AUTH_COLLECTION_PASSWORD"],
-        os.environ["AUTH_COLLECTION_PASSWORD_TADA"],
-        os.environ["AUTH_COLLECTION_PASSWORD_ASKGURUPUBLIC"],
-        os.environ["AUTH_COLLECTION_PASSWORD_ONECLICKCX"],
     ]:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
