de0ed8abad6a8e3e373464db957f3df52cf18c87
Alexander Smirnov <furiousteabag@gmail.com>
2023-08-23 03:07:29 +0300

    use user messages from chat as query

--- handlers/collection_handler.py
+++ handlers/collection_handler.py
@@ -22,6 +22,8 @@ from utils.schemas import (
     GetCollectionRankingResponse,
     GetCollectionResponse,
     GetCollectionsResponse,
+    Message,
+    Role,
     Source,
 )
 
@@ -38,15 +40,19 @@ class CollectionHandler:
         vendor: str,
         organization: str,
         collections: List[str],
-        query: str,
         api_version: ApiVersion,
         user_security_groups: List[int],
         project_to_en: bool,
+        query: str = None,
         document: str = None,
         document_collection: str = None,
         stream: bool = False,
         collections_only: bool = True,
+        chat: List[Message] = None,
     ) -> GetCollectionAnswerResponse:
+        if not query and chat:
+            query = "\n".join([msg.content for msg in chat if msg.role == Role.user])
+
         orig_lang = "en"
         if project_to_en:
             translation = AWS_TRANSLATE_CLIENT.translate_text(query)
@@ -94,7 +100,7 @@ class CollectionHandler:
             logger.info(f"answer_in_context: {answer_in_context}")
             if not answer_in_context:
                 context, mode = "", "general"
-        answer = await ml_requests.get_answer(context, query, api_version.value, mode=mode, stream=stream)
+        answer = await ml_requests.get_answer(context, query, api_version.value, mode=mode, stream=stream, chat=chat)
 
         sources, seen = [], set()
         for title, doc_id, doc_summary, collection in zip(
