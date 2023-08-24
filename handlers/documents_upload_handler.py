b1bb635f1beac143d2c41abd5000ffcd9ebf85c1
Sergei Gorbatiuk <spgorbatiuk@gmail.com>
2023-08-24 18:02:19 -0400

    removed unsafe cache in milvus manager

--- handlers/documents_upload_handler.py
+++ handlers/documents_upload_handler.py
@@ -183,6 +183,7 @@ class DocumentsUploadHandler:
                 logger.info(f"Deleted file {filename} from GridFS")
             else:
                 logger.error(f"File {filename} not found in GridFS for deletion")
+        collection.release()
         MILVUS_DB.delete_collection(full_collection_name)
         return CollectionDocumentsResponse(n_chunks=len(data))
 
